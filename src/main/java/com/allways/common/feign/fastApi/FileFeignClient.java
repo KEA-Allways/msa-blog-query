4196619ff6a87f7288828d78b652de5367ce2730
“mj” <75360915+minjun0707@users.noreply.github.com>
2023-12-01 17:53:58 +0900

    Squashed commit of the following:
    commit 2f028ca338d99bfc845e0f31a1b795f07b5747c3
    Author: “mj” <75360915+minjun0707@users.noreply.github.com>
    Date:   Fri Dec 1 02:01:01 2023 +0900
    
        fix : url 오타 수정
    
    commit 0a938819f6fc8458bcd2641d15f8e7462814f1d5
    Author: “mj” <75360915+minjun0707@users.noreply.github.com>
    Date:   Fri Dec 1 00:24:38 2023 +0900
    
        fix : feign url 환경변수으로 변경
    
    commit 270079230cd0413256baefcf2e621f2c908148b7
    Author: “mj” <75360915+minjun0707@users.noreply.github.com>
    Date:   Fri Dec 1 00:24:08 2023 +0900
    
        chore : feign url 환경변수 설정

--- src/main/java/com/allways/common/feign/fastApi/FileFeignClient.java
+++ src/main/java/com/allways/common/feign/fastApi/FileFeignClient.java
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "fastApi", url = "http://localhost:8001")
+@FeignClient(name = "fastApi", url = "${env.file-feign-url}")
 public interface FileFeignClient {
 
-    @PostMapping("/api/feign/post")
+    @PostMapping("/post")
     FileFeignResponse queryImageUrlByPost(@RequestBody UserByPostFeignRequest fileFeignRequest);
 
-    @PostMapping("/api/feign/post/list")
+    @PostMapping("/post/list")
     List<FileFeignResponse> queryImageUrlListByPost(@RequestBody List<UserByPostFeignRequest> urlByPostFeignRequestList);
 
-    @PostMapping("/api/feign/reply/list")
+    @PostMapping("/reply/list")
     List<FileFeignReplyResponse> queryImageUrlByReply(@RequestBody List<UserByReplyFeignRequest> userByReplyFeignRequestList);
 
 }
